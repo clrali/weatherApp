f15094c87fa6162b7deece562dd93e6f971eda0b
clrali <clara4695@gmail.com>
2022-01-30 17:58:07 -0500

    updated description for the app

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import requests
 import time
-
+# A simple GUI that displays the metrics pertaining to the weather for any city in the world.
+# Times are all presented in ET
 
 def getWeather(canvas):
     city = textfield.get()
